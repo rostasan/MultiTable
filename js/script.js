77ffff0d11c947dbfe0efb16446034bd994a5474
rostasan <ross.dickman@gmail.com>
2015-10-27 19:55:40 -0500

    Added Input field and logic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 /**
  * Created by 0 on 10/26/2015.
  */
-var app = angular.module('myApp',[])
-    .controller('MultiplicationCtrl', function($scope){
-        $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+angular.module('myApp', [])
+    .controller('MultiplicationCtrl', function($scope, $attrs) {
+        $scope.numberLimit = $attrs.initialNumberLimit || 10;
 
+        $scope.numbers = function() {
+            var numbers = [];
+            for(var i=0; i<$scope.numberLimit; i++) {
+                numbers[i] = i + 1;
+            };
+            return numbers;
+        };
 
+        $scope.compute = function(a,b) {
+            return a * b;
 
+        };
 
-});
+    });
