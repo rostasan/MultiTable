c637d0fc97f434622b2a7426709406d5e86c3006
rostasan <ross.dickman@gmail.com>
2015-10-27 19:24:15 -0500

    Added lesson code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by 0 on 10/26/2015.
  */
+var app = angular.module('myApp',[])
+    .controller('MultiplicationCtrl', function($scope){
+        $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+
+});
